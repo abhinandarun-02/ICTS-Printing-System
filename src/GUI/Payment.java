38cbd0e16657dfc661d99a3a6d61581ec684d202
rahul_nair <rahulp.nair.2504@gmail.com>
2021-12-15 10:55:56 +0530

    paymentPage monthly bill edited out
    removed month scrollpane, 
    added credit textfield.

--- src/GUI/Payment.java
+++ src/GUI/Payment.java
@@ -23,30 +23,28 @@ class Payment extends JFrame implements ActionListener {
     JLabel id;
     JTextField billTextField;
 
-    JLabel month;
-    String[] list;
-    JList<String> select;
-    JScrollPane sp;
+    
     JButton submit;
 
     JLabel amount;
+    JLabel credit;
     JTextField amountTextField;
+    JTextField creditTextField;
 
     JLabel note;
 
     Payment() {
 
-        bill = new JLabel("View Monthly bill");
+        bill = new JLabel("View bill");
         id = new JLabel("Employee ID");
         billTextField = new JTextField();
-        month = new JLabel("Month");
-        list = new String[]{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-        select = new JList<>(list);
-        sp = new JScrollPane(select);
+        
         submit = new JButton("SUBMIT");
-        amount = new JLabel("Amount for the given month");
+        amount = new JLabel("Amount :");
+        credit = new JLabel("Credits left :");
         amountTextField = new JTextField();
-        note = new JLabel("Please Login to view full payment history");
+        creditTextField = new JTextField();
+        note = new JLabel("Please Login to view more details");
 
         //Calling methods inside constructor.
         setLayoutManager();
@@ -68,26 +66,29 @@ class Payment extends JFrame implements ActionListener {
     public void setLocationAndSize() {
         //Setting location and Size of each component using setBounds() method.
 
-        bill.setBounds(100, 100, 300, 30);
+        bill.setBounds(140, 100, 300, 30);
         id.setBounds(250, 150, 100, 30);
         billTextField.setBounds(400, 150, 100, 20);
 
-        month.setBounds(250, 200, 100, 30);
-        sp.setBounds(400, 200, 100, 25);
-        submit.setBounds(300, 270, 100, 20);
+        
+        submit.setBounds(310, 230, 100, 20);
 
-        amount.setBounds(170, 360, 200, 30);
-        amountTextField.setBounds(420, 360, 150, 20);
+        amount.setBounds(170, 330, 200, 30);
+        credit.setBounds(170, 370, 200, 30);
+        amountTextField.setBounds(420, 330, 150, 20);
         amountTextField.setEditable(false);
+        creditTextField.setBounds(420, 370, 150, 20);
+        creditTextField.setEditable(false);
 
-        note.setBounds(240, 410, 300, 30);
+        note.setBounds(260, 440, 300, 30);
     }
 
     public void setFont() {
         bill.setFont(new Font("Calibri", Font.BOLD, 16));
         id.setFont(new Font("Calibri", Font.PLAIN, 16));
-        month.setFont(new Font("Calibri", Font.PLAIN, 16));
+        
         amount.setFont(new Font("Calibri", Font.PLAIN, 16));
+        credit.setFont(new Font("Calibri", Font.PLAIN, 16));
     }
 
     public void addComponentsToContainer() {
@@ -97,11 +98,11 @@ class Payment extends JFrame implements ActionListener {
         panel.add(id);
         panel.add(billTextField);
         panel.add(bill);
-        panel.add(month);
-        panel.add(sp);
         panel.add(submit);
         panel.add(amount);
+        panel.add(credit);
         panel.add(amountTextField);
+        panel.add(creditTextField);
         panel.add(note);
 
         TitledBorder titledBorder = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.gray, 3), "PAYMENT PORTAL", TitledBorder.CENTER, TitledBorder.CENTER, new Font("Serif", Font.PLAIN, 18));
