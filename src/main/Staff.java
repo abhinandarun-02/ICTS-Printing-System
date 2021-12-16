97a268dda7c3ffce29727b8dc6ed4648f350060a
Abhinand Arun <abhinandarun2015@gmail.com>
2021-12-17 02:43:34 +0530

    Implement methods for fetching tables in Staff

--- src/main/Staff.java
+++ src/main/Staff.java
@@ -1,4 +1,47 @@
 package main;
 
-public abstract class Staff extends Person{
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public abstract class Staff extends Person {
+
+
+    public ResultSet getRequestTable() {
+        Connection connection;
+        Statement st;
+        ResultSet rs = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/printing-system", "root", "root");
+            st = connection.createStatement();
+            String query = "SELECT  print_id, name, employee_id, page_type, paper_type, colour_type, date FROM request_details;";
+            rs = st.executeQuery(query);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    public ResultSet getPrintTable() {
+        Connection connection;
+        Statement st;
+        ResultSet rs = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/printing-system", "root", "root");
+            st = connection.createStatement();
+            String query = "select r.print_id, r.employee_id, r.paper_type, r.colour_type, r.page_type, p.no_of_pages, no_of_copies from request_details r ,print_details p;";
+            rs = st.executeQuery(query);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
 }
