ff18b1782a5ba74fc9bc379470aa3fc97075f9a5
Abhinand Arun <abhinandarun2015@gmail.com>
2021-12-17 13:39:34 +0530

    Update sql query

--- src/GUI/ClerksPage.java
+++ src/GUI/ClerksPage.java
@@ -463,7 +463,6 @@ public class ClerksPage extends JFrame implements ActionListener {
 
         int i = 0;
         ResultSet rs = user.getPrintTable();
-        System.out.println(queueModel.getColumnCount());
         try {
             while (rs.next()) {
                 queueModel.insertRow(i, new Object[]{rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6), rs.getInt(7)});
