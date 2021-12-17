d32fb230f372d1d5709d2edcbdabfb97a5b7a1ce
rahul_nair <rahulp.nair.2504@gmail.com>
2021-12-17 16:43:16 +0530

    Remove viewAmount/Credit from Employee

--- src/main/Employee.java
+++ src/main/Employee.java
@@ -11,7 +11,6 @@ public class Employee extends Person {
     private String roomNo;
     private String department;
     
-    private Connection connection;
     
     public int usageCount;
     public int requestCount;
@@ -20,60 +19,7 @@ public class Employee extends Person {
     public String getRoomNo() {
         return roomNo;
     }
-
-    public double viewAmount(String emp_id)
-    {
-    	ResultSet rs =null;
-    	 try {
-             Class.forName("org.postgresql.Driver");
-             connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/printing-system", "root", "root");
-         } 
-    	 catch (SQLException | ClassNotFoundException e) {
-             e.printStackTrace();
-         }
-    	 try {
-    		 Statement s = connection.createStatement();
-    		 String query = "SELECT amount FROM EMPLOYEE WHERE employee_id= '"+ emp_id+ "';";
-    		 rs = s.executeQuery(query);
-             while(rs.next())
-             {
-            	 return(rs.getDouble("amount"));
-             }
-             
-    	 }
-    	 catch(SQLException e){
-    		 e.printStackTrace();
-    	 }
-    	 return -1;    	
-    }
     
-    public double viewCredit(String emp_id)
-    {
-    	ResultSet rs =null;
-    	 try {
-             Class.forName("org.postgresql.Driver");
-             connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/printing-system", "root", "root");
-         } 
-    	 catch (SQLException | ClassNotFoundException e) {
-             e.printStackTrace();
-         }
-    	 try {
-    		 Statement s = connection.createStatement();
-    		 String query = "SELECT credits FROM EMPLOYEE WHERE employee_id= '"+ emp_id+ "';";
-    		 rs = s.executeQuery(query);
-             while(rs.next())
-             {
-            	 return(rs.getDouble("credits"));
-             }
-             
-    	 }
-    	 catch(SQLException e){
-    		 e.printStackTrace();
-    	 }
-    	 return -1;    	
-    }
-    
-
     public void payBill() {
 
     }
