1277711b3695a2ba781a1532ac1818bf1ebb7aae
Abhinand Arun <abhinandarun2015@gmail.com>
2021-12-18 00:52:07 +0530

    Implement generateBill() method in Clerk

--- src/main/Clerk.java
+++ src/main/Clerk.java
@@ -51,7 +51,20 @@ public class Clerk extends Staff {
 
     }
 
-    public void generateBill() {
+    public void generateBill(String print_id, int cost_per_page, int no_of_pages, int no_of_copies) {
+        Connection connection = getConnection();
+        int total_cost = cost_per_page * no_of_pages * no_of_copies;
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE print_details SET cost_per_pg = ?, no_of_pages = ?, no_of_copies = ?, total_cost = ?, status = 'Print Completed' WHERE print_id = ?;");
+            preparedStatement.setInt(1, cost_per_page);
+            preparedStatement.setInt(2, no_of_pages);
+            preparedStatement.setInt(3, no_of_copies);
+            preparedStatement.setInt(4, total_cost);
+            preparedStatement.setString(5, print_id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
