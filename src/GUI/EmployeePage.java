435afc041fb9f1864e57ff0d450b6302bf8fdd23
Abhinand Arun <abhinandarun2015@gmail.com>
2021-12-18 18:51:11 +0530

    Implement delete Request feature
    Co-authored-by: Abhinand Arun <abhinandarun2015@gmail.com>

--- src/GUI/EmployeePage.java
+++ src/GUI/EmployeePage.java
@@ -1,4 +1,5 @@
 package GUI;
+
 import main.Employee;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,130 +14,150 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 class EmployeePage extends JFrame implements ActionListener {
-	
-    Employee user = new Employee();
-    String username;
-    JLabel status;
-    JLabel id1;
-    JTextField statusTextField;
-    JButton submit1;
 
-    JLabel req;
-    JLabel id2;
-    JTextField reqTextField;
-    JButton submit2;
+	Employee user = new Employee();
+	String username;
+	JLabel status;
+	JLabel id1;
+	JTextField statusTextField;
+	JButton submit1;
 
-    JLabel bill;
-    JLabel amount;
-    JTextField amountField;
-    JTextField creditsField;
-    JLabel credits;
-    String[] list;
-    JButton submit3;
+	JLabel req;
+	JLabel id2;
+	JTextField reqTextField;
+	JButton submit2;
 
-    String[] column;
-    String[][] data;
-    JTable table;
-    JScrollPane sp;
+	JLabel bill;
+	JLabel amount;
+	JTextField amountField;
+	JTextField creditsField;
+	JLabel credits;
+	String[] list;
+	JButton submit3;
 
-    EmployeePage(String u) {
-    	
-        this.username = u;
-        status = new JLabel("CHECK STATUS");
-        id1 = new JLabel("REQUEST ID");
-        statusTextField = new JTextField();
-        submit1 = new JButton("SUBMIT");
+	String[] column;
+	String[][] data;
+	JTable table;
+	JScrollPane sp;
 
-        req = new JLabel("DELETE REQUEST");
-        id2 = new JLabel("Request ID");
-        reqTextField = new JTextField();
-        submit2 = new JButton("SUBMIT");
+	EmployeePage(String u) {
 
-        bill = new JLabel("VIEW AMOUNT/CREDITS");
-        amount = new JLabel("Amount:");
-        credits = new JLabel("Credits:");
-        submit3 = new JButton("SUBMIT");
-        amountField = new JTextField("");
-        creditsField = new JTextField("");
+		this.username = u;
+		status = new JLabel("CHECK STATUS");
+		id1 = new JLabel("REQUEST ID");
+		statusTextField = new JTextField();
+		submit1 = new JButton("SUBMIT");
 
-        column = new String[]{"Print ID", "Date", "Time", "Use", "Status", "Credit"};
-        data = new String[][]{};
-        table = new JTable(data, column);
-        sp = new JScrollPane(table);
+		req = new JLabel("DELETE REQUEST");
+		id2 = new JLabel("Request ID");
+		reqTextField = new JTextField();
+		submit2 = new JButton("SUBMIT");
 
+		bill = new JLabel("VIEW AMOUNT/CREDITS");
+		amount = new JLabel("Amount:");
+		credits = new JLabel("Credits:");
+		submit3 = new JButton("SUBMIT");
+		amountField = new JTextField("");
+		creditsField = new JTextField("");
 
-        getContentPane().setLayout(null);
+		column = new String[] { "Print ID", "Date", "Time", "Use", "Status", "Credit" };
+		data = new String[][] {};
+		table = new JTable(data, column);
+		sp = new JScrollPane(table);
 
-        setTitle("Employee Login");
-        setBounds(150, 75, 1015, 700);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-        setResizable(false);
+		getContentPane().setLayout(null);
 
-        sp.setBounds(200, 100, 700, 100);
+		setTitle("Employee Login");
+		setBounds(270, 75, 1015, 700);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setResizable(false);
 
-        status.setBounds(200, 220, 100, 30);
-        id1.setBounds(400, 260, 100, 30);
-        statusTextField.setBounds(550, 260, 100, 20);
-        submit1.setBounds(480, 300, 100, 20);
+		sp.setBounds(200, 100, 700, 100);
 
-        req.setBounds(200, 350, 150, 30);
-        id2.setBounds(400, 390, 100, 30);
-        reqTextField.setBounds(550, 390, 100, 20);
-        submit2.setBounds(480, 430, 100, 20);
+		status.setBounds(200, 220, 100, 30);
+		id1.setBounds(400, 260, 100, 30);
+		statusTextField.setBounds(550, 260, 100, 20);
+		submit1.setBounds(480, 300, 100, 20);
 
-        bill.setBounds(200, 480, 180, 30);
-        amount.setBounds(400, 510, 100, 30);
-        amountField.setBounds(510, 509, 100, 30);
-        credits.setBounds(400, 550, 100, 30);
-        creditsField.setBounds(510, 550, 100, 30);
-        submit3.setBounds(480, 600, 100, 20);
+		req.setBounds(200, 350, 150, 30);
+		id2.setBounds(400, 390, 100, 30);
+		reqTextField.setBounds(550, 390, 100, 20);
+		submit2.setBounds(480, 430, 100, 20);
 
+		bill.setBounds(200, 480, 180, 30);
+		amount.setBounds(400, 510, 100, 30);
+		amountField.setBounds(510, 509, 100, 30);
+		credits.setBounds(400, 550, 100, 30);
+		creditsField.setBounds(510, 550, 100, 30);
+		submit3.setBounds(480, 600, 100, 20);
 
-        status.setFont(new Font("Calibri", Font.BOLD, 16));
-        id1.setFont(new Font("Calibri", Font.PLAIN, 16));
-        req.setFont(new Font("Calibri", Font.BOLD, 16));
-        id2.setFont(new Font("Calibri", Font.PLAIN, 16));
-        bill.setFont(new Font("Calibri", Font.BOLD, 16));
-        credits.setFont(new Font("Calibri", Font.PLAIN, 16));
-        amount.setFont(new Font("Calibri", Font.PLAIN, 16));
+		status.setFont(new Font("Calibri", Font.BOLD, 16));
+		id1.setFont(new Font("Calibri", Font.PLAIN, 16));
+		req.setFont(new Font("Calibri", Font.BOLD, 16));
+		id2.setFont(new Font("Calibri", Font.PLAIN, 16));
+		bill.setFont(new Font("Calibri", Font.BOLD, 16));
+		credits.setFont(new Font("Calibri", Font.PLAIN, 16));
+		amount.setFont(new Font("Calibri", Font.PLAIN, 16));
 
-        //Adding each component to the Container
-        getContentPane().add(sp);
+		// Adding each component to the Container
+		getContentPane().add(sp);
 
-        getContentPane().add(status);
-        getContentPane().add(id1);
-        getContentPane().add(statusTextField);
-        getContentPane().add(submit1);
+		getContentPane().add(status);
+		getContentPane().add(id1);
+		getContentPane().add(statusTextField);
+		getContentPane().add(submit1);
 
-        getContentPane().add(req);
-        getContentPane().add(id2);
-        getContentPane().add(reqTextField);
-        getContentPane().add(submit2);
+		getContentPane().add(req);
+		getContentPane().add(id2);
+		getContentPane().add(reqTextField);
+		getContentPane().add(submit2);
 
-        getContentPane().add(bill);
-        getContentPane().add(credits);
-        getContentPane().add(amount);
-        getContentPane().add(amountField);
-        getContentPane().add(creditsField);
-        getContentPane().add(submit3);
-        submit3.addActionListener(this);
+		getContentPane().add(bill);
+		getContentPane().add(credits);
+		getContentPane().add(amount);
+		getContentPane().add(amountField);
+		getContentPane().add(creditsField);
+		getContentPane().add(submit3);
+		submit3.addActionListener(this);
+		submit2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String ans = reqTextField.getText();
+				int opt = JOptionPane.showConfirmDialog(null, "Are You Sure?", "CONFIRM", JOptionPane.YES_NO_OPTION,
+						JOptionPane.QUESTION_MESSAGE);
+				if (opt == JOptionPane.YES_OPTION) {
+					int res = user.deleteRequest(username, ans);
+					if (res == 1)
+						{JOptionPane.showMessageDialog(null, "Request deleted. Deletion successful");
+						 reqTextField.setText("");
+						}
+					else if (res == 0)
+						JOptionPane.showMessageDialog(null, "Print already in queue", "Deletion period exceeded",
+								JOptionPane.WARNING_MESSAGE);
+					else if (res == 2)
+						JOptionPane.showMessageDialog(null, "Request ID does not correspond to your Employee ID",
+								"Invalid request", JOptionPane.ERROR_MESSAGE);
+					else if(res==-1)
+						JOptionPane.showMessageDialog(null, "Record not found ", "Error", JOptionPane.ERROR_MESSAGE);
+					else
+						JOptionPane.showMessageDialog(null, "Deletion failed", "Error!!", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
 
-    }
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        double x = user.viewAmount(username);
-        double y = user.viewCredit(username);
-        amountField.setText(String.valueOf(x));
-        creditsField.setText(String.valueOf(y));
-        
-    
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		double x = user.viewAmount(username);
+		double y = user.viewCredit(username);
+		amountField.setText(String.valueOf(x));
+		creditsField.setText(String.valueOf(y));
 
-    }
+	}
 
-    public static void main(String[] args) {
-        new EmployeePage("/null");
-    }
+	public static void main(String[] args) {
+		new EmployeePage("/null");
+	}
 
 }
--- src/main/Employee.java
+++ src/main/Employee.java
@@ -2,41 +2,95 @@ package main;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Employee extends Person {
 
-    private String roomNo;
-    private String department;
-    
-    
-    public int usageCount;
-    public int requestCount;
-    public int pendingCash;
+	private String roomNo;
+	private String department;
+	private Connection connection;
 
-    public String getRoomNo() {
-        return roomNo;
-    }
-    
-    public void payBill() {
+	public int usageCount;
+	public int requestCount;
+	public int pendingCash;
 
-    }
+	public String getRoomNo() {
+		return roomNo;
+	}
 
-    public void requestPrint() {
+	public void payBill() {
 
-    }
+	}
 
-    public void checkStatus() {
+	public void requestPrint() {
 
-    }
+	}
 
-    public void viewPrintHistory() {
+	public void checkStatus() {
 
-    }
+	}
 
-    public void deleteRequest() {
+	public void viewPrintHistory() {
 
-    }
+	}
+
+	public int deleteRequest(String username, String req_id) {
+
+		String query0 = "select count(print_id) from print_details where print_id='"+req_id+"';";
+		String query1 = "select employee_id from employee where username='" + username + "';";
+		String query2 = "select employee_id from print_details where print_id='" + req_id + "';";
+		String query4 = "select status from print_details where print_id='" + req_id + "';";
+		String query5 = "update print_details set status ='Deleted' where print_id='" + req_id + "';";
+		String emp_id = "/", emp_id2 = "/", status = "";
+		int count=-1;
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/printing-system", "root",
+					"root");
+
+			Statement stmt = connection.createStatement();
+			ResultSet rsp = stmt.executeQuery(query0);
+			while (rsp.next()) {
+				count = rsp.getInt("count");
+			}
+			rsp.close();
+			if(count==1)
+			{
+			ResultSet rs = stmt.executeQuery(query1);
+			while (rs.next()) {
+				emp_id = rs.getString("employee_id");
+			}
+			rs.close();
+			ResultSet rs2 = stmt.executeQuery(query2);
+			while (rs2.next()) {
+				emp_id2 = rs2.getString("employee_id");
+			}
+			rs2.close();
+			if (emp_id.equals(emp_id2)) {
+				ResultSet rs3 = stmt.executeQuery(query4);
+				while (rs3.next()) {
+					status = rs3.getString("status");
+				}
+				rs3.close();
+				if (status.equals("Pending")) 
+				{
+					stmt.executeUpdate(query5);
+					return 1;
+				}
+				else
+					return 0;
+			} else
+				return 2;
+			}
+			else 
+				return -1;
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return -2;
+	}
+
 }
