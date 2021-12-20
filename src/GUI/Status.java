bfc9ea9f0b26f4671a643f8375fa8a12f2e0bd9f
rahul_nair <rahulp.nair.2504@gmail.com>
2021-12-20 19:43:14 +0530

    Implement and overload checkStatus
    checkStatus/Employee page implemented
    bug fix in Person/checkStatus
    deleteRequest code trimmed

--- src/GUI/Status.java
+++ src/GUI/Status.java
@@ -4,6 +4,7 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
@@ -17,90 +18,96 @@ import main.Person;
 
 class Status extends JFrame implements ActionListener {
 
-    JPanel panel = (JPanel) getContentPane();
+	JPanel panel = (JPanel) getContentPane();
 
-    JLabel id;
-    JTextField idTextField;
-    JButton submit;
+	JLabel id;
+	JTextField idTextField;
+	JButton submit;
 
-    JLabel status;
-    JTextField statusTextField;
+	JLabel status;
+	JTextField statusTextField;
 
-    Person user;
+	Person user;
 
-    Status() {
+	Status() {
 
-        user = new Person();
+		user = new Person();
 
-        id = new JLabel("Request ID");
-        idTextField = new JTextField();
-        submit = new JButton("SUBMIT");
-        status = new JLabel("Status of Print:  ");
-        statusTextField = new JTextField();
+		id = new JLabel("Request ID");
+		idTextField = new JTextField();
+		submit = new JButton("SUBMIT");
+		status = new JLabel("Status of Print:  ");
+		statusTextField = new JTextField();
 
-        submit.addActionListener(this);
+		submit.addActionListener(this);
 
-        //Calling methods inside constructor.
-        setLayoutManager();
-        setLocationAndSize();
-        setFont();
-        addComponentsToContainer();
-        setTitle("Status");
-        setBounds(450, 75, 600, 450);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setVisible(true);
-        setResizable(false);
+		//Calling methods inside constructor.
+		setLayoutManager();
+		setLocationAndSize();
+		setFont();
+		addComponentsToContainer();
+		setTitle("Status");
+		setBounds(450, 75, 600, 450);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+		setResizable(false);
 
-    }
+	}
 
-    public void setLayoutManager() {
-        panel.setLayout(null);
-    }
+	public void setLayoutManager() {
+		panel.setLayout(null);
+	}
 
-    public void setLocationAndSize() {
-        //Setting location and Size of each component using setBounds() method.
+	public void setLocationAndSize() {
+		//Setting location and Size of each component using setBounds() method.
 
-        id.setBounds(150, 100, 100, 30);
-        idTextField.setBounds(300, 100, 100, 20);
-        submit.setBounds(230, 160, 100, 20);
+		id.setBounds(150, 100, 100, 30);
+		idTextField.setBounds(300, 100, 100, 20);
+		submit.setBounds(230, 160, 100, 20);
 
-        status.setBounds(120, 260, 200, 30);
-        statusTextField.setBounds(280, 260, 150, 20);
-        statusTextField.setEditable(false);
-    }
+		status.setBounds(120, 260, 200, 30);
+		statusTextField.setBounds(280, 260, 150, 20);
+		statusTextField.setEditable(false);
+	}
 
-    public void setFont() {
-        id.setFont(new Font("Calibri", Font.PLAIN, 16));
-        status.setFont(new Font("Calibri", Font.PLAIN, 16));
-    }
+	public void setFont() {
+		id.setFont(new Font("Calibri", Font.PLAIN, 16));
+		status.setFont(new Font("Calibri", Font.PLAIN, 16));
+	}
 
-    public void addComponentsToContainer() {
-        //Adding each component to the Container
+	public void addComponentsToContainer() {
+		//Adding each component to the Container
 
-        panel.add(id);
-        panel.add(idTextField);
-        panel.add(submit);
+		panel.add(id);
+		panel.add(idTextField);
+		panel.add(submit);
 
-        panel.add(status);
-        panel.add(statusTextField);
-
-
-        TitledBorder titledBorder = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.gray, 3), "CHECK STATUS", TitledBorder.CENTER, TitledBorder.CENTER, new Font("Serif", Font.PLAIN, 18));
-        panel.setBorder(titledBorder);
-    }
+		panel.add(status);
+		panel.add(statusTextField);
 
 
-    public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("SUBMIT")) {
-            String print_id = idTextField.getText();
-            String status = user.checkStatus(print_id);
-            statusTextField.setText(status);
-        }
-    }
+		TitledBorder titledBorder = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.gray, 3), "CHECK STATUS", TitledBorder.CENTER, TitledBorder.CENTER, new Font("Serif", Font.PLAIN, 18));
+		panel.setBorder(titledBorder);
+	}
 
-    public static void main(String[] args) {
-        new Status();
-    }
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("SUBMIT")) {
+			String print_id = idTextField.getText();
+			String status = user.checkStatus(print_id);
+			if(status.equals("NotFound"))
+			{
+				JOptionPane.showMessageDialog(submit, "Request not found! ",
+						"Request ID invalid", JOptionPane.ERROR_MESSAGE);
+			}
+			else
+				statusTextField.setText(status);
+		}
+	}
+
+	public static void main(String[] args) {
+		new Status();
+	}
 }
 
 
--- src/main/Employee.java
+++ src/main/Employee.java
@@ -9,90 +9,111 @@ import java.sql.Statement;
 
 public class Employee extends Person {
 
-    private String department;
-    
-/*TODO*/
-//    private int usageCount;
-//    private int requestCount;
-//    private int pendingCash;
+	private String department;
 
-    public Employee() {
+	/* TODO */
+	// private int usageCount;
+	// private int requestCount;
+	// private int pendingCash;
 
-    }
+	public Employee() {
 
-    public Employee(String username) {
-        super(username);
-        Connection connection = getConnection();
-        try {
-            PreparedStatement preparedstatement1 = connection.prepareStatement("SELECT dept_name from employee WHERE username = ?");
-            preparedstatement1.setString(1, username);
-            ResultSet resultSet1 = preparedstatement1.executeQuery();
-            if (resultSet1.next()) this.department = resultSet1.getString(1);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+	}
 
+	public Employee(String username) {
+		super(username);
+		Connection connection = getConnection();
+		try {
+			PreparedStatement preparedstatement = connection
+					.prepareStatement("SELECT dept_name from employee WHERE username = ?");
+			preparedstatement.setString(1, username);
+			ResultSet resultSet = preparedstatement.executeQuery();
+			if (resultSet.next())
+				this.department = resultSet.getString(1);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void checkStatus() {
+	public String checkStatus(String print_id, String username) {
 
-    }
+		ResultSet resultSet1;
+		ResultSet resultSet2;
+		String e1 = "", e2 = "";
+		try {
+			String status = super.checkStatus(print_id);
+			Connection connection = getConnection();
+			PreparedStatement preparedStatement1 = connection
+					.prepareStatement("select employee_id from employee where username=?;");
+			PreparedStatement preparedStatement2 = connection
+					.prepareStatement("select employee_id from print_details where print_id=?;");
 
-    public void viewPrintHistory() {
+			preparedStatement1.setString(1, username);
+			resultSet1 = preparedStatement1.executeQuery();
+			if (resultSet1.next()) {
+				e1 = resultSet1.getString(1);
+			}
+			preparedStatement2.setString(1, print_id);
+			resultSet2 = preparedStatement2.executeQuery();
+			if (resultSet2.next()) {
+				e2 = resultSet2.getString(1);
+			} else
+				return "NotFound";
+			if (e1.equals(e2)) {
+				return status;
+			} else {
+				return "NoMatch";
+			}
+		} catch (Exception e) {
 
-    }
+			e.printStackTrace();
+		}
+		return "NotFound";
 
-    public int deleteRequest(String username, String req_id) {
+	}
 
-        String query0 = "select count(print_id) from print_details where print_id='" + req_id + "';";
-        String query1 = "select employee_id from employee where username='" + username + "';";
-        String query2 = "select employee_id from print_details where print_id='" + req_id + "';";
-        String query4 = "select status from print_details where print_id='" + req_id + "';";
-        String query5 = "update print_details set status ='Deleted' where print_id='" + req_id + "';";
-        String emp_id = "/", emp_id2 = "/", status = "";
-        int count = -1;
-        try {
-            Connection connection = getConnection();
+	public void viewPrintHistory() {
 
-            Statement stmt = connection.createStatement();
-            ResultSet rsp = stmt.executeQuery(query0);
-            while (rsp.next()) {
-                count = rsp.getInt("count");
-            }
-            rsp.close();
-            if (count == 1) {
-                ResultSet rs = stmt.executeQuery(query1);
-                while (rs.next()) {
-                    emp_id = rs.getString("employee_id");
-                }
-                rs.close();
-                ResultSet rs2 = stmt.executeQuery(query2);
-                while (rs2.next()) {
-                    emp_id2 = rs2.getString("employee_id");
-                }
-                rs2.close();
-                if (emp_id.equals(emp_id2)) {
-                    ResultSet rs3 = stmt.executeQuery(query4);
-                    while (rs3.next()) {
-                        status = rs3.getString("status");
-                    }
-                    rs3.close();
-                    if (status.equals("Pending")) {
-                        stmt.executeUpdate(query5);
-                        return 1;
-                    }
-                    else
-                        return 0;
-                }
-                else
-                    return 2;
-            }
-            else
-                return -1;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return -2;
-    }
+	}
+
+	public int deleteRequest(String username, String req_id) {
+
+		String query1 = "select employee_id from employee where username='" + username + "';";
+		String query2 = "select employee_id from print_details where print_id='" + req_id + "';";
+		String query4 = "select status from print_details where print_id='" + req_id + "';";
+		String query5 = "update print_details set status ='Deleted' where print_id='" + req_id + "';";
+		String emp_id = "", emp_id2 = "", status = "";
+
+		try {
+			Connection connection = getConnection();
+
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query1);
+			if (rs.next()) {
+				emp_id = rs.getString("employee_id");
+			}
+			ResultSet rs2 = stmt.executeQuery(query2);
+			if (rs2.next()) {
+				emp_id2 = rs2.getString("employee_id");
+			}
+			if (emp_id.equals(emp_id2)) {
+				ResultSet rs3 = stmt.executeQuery(query4);
+				if (rs3.next()) {
+					status = rs3.getString("status");
+				} else
+					return -1;
+				if (status.equals("Pending")) {
+					stmt.executeUpdate(query5);
+					return 1;
+				} else
+					return 0;
+			} else
+				return 2;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return -2;
+	}
 
 }
