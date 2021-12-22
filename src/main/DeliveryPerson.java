9a9a1dfa3c149f8bb0910d63f8b3fca9c2fe2b94
Abhinand Arun <abhinandarun2015@gmail.com>
2021-12-22 17:13:19 +0530

    Implement methods in DeliveryPerson Class

--- src/main/DeliveryPerson.java
+++ src/main/DeliveryPerson.java
@@ -1,16 +1,39 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DeliveryPerson extends Person {
 
     private String deliveryCompleted;
     private String deliveryPending;
 
-    public void checkDeliveryQueue() {
+    public ResultSet checkDeliveryQueue() {
+        ResultSet rs = null;
+        try {
+            Connection connection = getConnection();
+            Statement st = connection.createStatement();
+            String query = "select print_id, employee_id, status from print_details where status = 'To be Delivered'";
+            rs = st.executeQuery(query);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rs;
     }
 
-    public void updateDeliveryStatus() {
-
+    public void updateDeliveryStatus(String print_id) {
+        Connection connection = getConnection();
+        Statement st = null;
+        try {
+            st = connection.createStatement();
+            String qeury = "update print_details set status = 'Delivered' where print_id = '" + print_id + "'";
+            st.executeUpdate(qeury);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
