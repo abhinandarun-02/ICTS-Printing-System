a56c3ddbf840a2562099b82a213ba9677a6cb89e
rahul_nair <rahulp.nair.2504@gmail.com>
2021-12-23 21:09:34 +0530

    Complete deliveryPage
    deliveryPage table model updated
    deliveryPerson functions implemented

--- src/GUI/DeliveryPage.java
+++ src/GUI/DeliveryPage.java
@@ -1,10 +1,12 @@
 package GUI;
-
+import main.DeliveryPerson;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -15,44 +17,84 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 class DeliveryPage extends JFrame implements ActionListener {
-
+	
+    DeliveryPerson dguy = new DeliveryPerson();
     JPanel panel = (JPanel) getContentPane();
-
-    String[] column;
-    String[][] data;
-    JTable table;
-    JScrollPane sp;
-
     JLabel status;
+    JScrollPane sp;
     JLabel id;
     JTextField statusTextField;
     JButton submit;
+    DefaultTableModel delivery;
+	JTable deliveryTable;
+	JLabel statusLabel;
+    
     private JLabel pendingRequestsLabel;
 
     DeliveryPage() {
-        column = new String[]{"Print ID", "User ID", "Name", "Room no", "Phone no", "Bill Status"};
-        data = new String[][]{};
-        table = new JTable(data, column);
-        table.setFont(new Font("Arial", Font.PLAIN, 16));
-        table.setModel(new DefaultTableModel(
-        	new Object[][] {
-        	},
-        	new String[] {
-        		"Print ID", "User ID", "Name", "Room no", "Phone no", "Status"
-        	}
-        ));
-        sp = new JScrollPane(table);
+ 
         status = new JLabel("UPDATE STATUS");
         id = new JLabel("Print ID :");
         statusTextField = new JTextField();
-        submit = new JButton("Update Status to DELIVERED");
+        submit = new JButton("Update Status");
+        
+        statusLabel = new JLabel("Set Status:");
+        statusLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+
+        String[] options = new String[]{"Delivered", "Out for delivery", "File missing"};
+        JComboBox<String> statusCB = new JComboBox<>(options);
+        statusCB.setFont(new Font("Arial", Font.PLAIN, 16));
+        statusCB.setBounds(810, 495, 150, 30);
+        this.add(statusCB);
         submit.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
+        		int opt = JOptionPane.showConfirmDialog(null, "Are You Sure?", "CONFIRM", JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+        		if(opt==JOptionPane.YES_OPTION)
+        		{
+        		int res=dguy.updateDeliveryStatus(statusTextField.getText(),statusCB.getSelectedItem().toString());
+        		if(res==1)
+        		{
+        			JOptionPane.showMessageDialog(null, "Status updated successfully!");
+					statusTextField.setText("");
+        		}
+        		else if(res==-1)
+        		{
+        			JOptionPane.showMessageDialog(null, "Print ID not found!","Invalid ID", JOptionPane.ERROR_MESSAGE);
+        		}
+        		}
+        		
         	}
         });
+    	
+    	delivery = new DefaultTableModel();
+		deliveryTable = new JTable(delivery);
+		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+		centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+		deliveryTable.setDefaultRenderer(String.class, centerRenderer);
+
+		delivery.addColumn("Print ID");
+		delivery.addColumn("Clerk ID");
+		delivery.addColumn("User ID");
+		delivery.addColumn("Name");
+		delivery.addColumn("Room.No");
+		delivery.addColumn("Department");
+		delivery.addColumn("Phone No.");
+		delivery.addColumn("Cost");
+	
+
+    	this.loadDeliveryTable();
+
+		for (int x = 0; x < deliveryTable.getColumnCount(); x++) {
+			deliveryTable.getColumnModel().getColumn(x).setCellRenderer(centerRenderer);
+		}
+   
+		sp = new JScrollPane(deliveryTable, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); 	
         submit.setFont(new Font("Arial", Font.PLAIN, 20));
 
 
@@ -80,14 +122,16 @@ class DeliveryPage extends JFrame implements ActionListener {
         sp.setBounds(50, 120, 1125, 250);
 
         status.setBounds(315, 415, 260, 42);
-        id.setBounds(448, 495, 127, 30);
-        statusTextField.setBounds(605, 495, 175, 30);
+        id.setBounds(315, 495, 127, 30);
+        statusTextField.setBounds(445, 495, 175, 30);
+        statusLabel.setBounds(665, 495, 175, 30);
         submit.setBounds(448, 582, 332, 54);
     }
 
     public void setFont() {
         status.setFont(new Font("Calibri", Font.PLAIN, 28));
         id.setFont(new Font("SansSerif", Font.PLAIN, 24));
+        statusLabel.setFont(new Font("SansSerif", Font.PLAIN, 24));
     }
 
     public void addComponentsToContainer() {
@@ -97,6 +141,7 @@ class DeliveryPage extends JFrame implements ActionListener {
         panel.add(status);
         panel.add(id);
         panel.add(statusTextField);
+        panel.add(statusLabel);
         panel.add(submit);
         
         pendingRequestsLabel = new JLabel("PENDING REQUESTS");
@@ -116,5 +161,21 @@ class DeliveryPage extends JFrame implements ActionListener {
     public static void main(String[] a) {
         new DeliveryPage();
     }
+    
+   public void loadDeliveryTable() {
+
+		int i = 0;
+	    ResultSet rs = dguy.checkDeliveryQueue();
+		try {
+			while (rs.next()) {
+				delivery.insertRow(i, new Object[]{rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6),rs.getString(7), rs.getString(8)});
+				i++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+    
+    
 }
 
--- src/main/DeliveryPerson.java
+++ src/main/DeliveryPerson.java
@@ -7,33 +7,52 @@ import java.sql.Statement;
 
 public class DeliveryPerson extends Person {
 
-    private String deliveryCompleted;
-    private String deliveryPending;
+	private String deliveryCompleted;
+	private String deliveryPending;
 
-    public ResultSet checkDeliveryQueue() {
-        ResultSet rs = null;
-        try {
-            Connection connection = getConnection();
-            Statement st = connection.createStatement();
-            String query = "select print_id, employee_id, status from print_details where status = 'To be Delivered'";
-            rs = st.executeQuery(query);
+	public ResultSet checkDeliveryQueue() {
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return rs;
-    }
+		int i=0;
+		ResultSet rs=null;
+		try {
+			Connection connection = getConnection();
+			Statement st = connection.createStatement();
+			String query = "select l.print_id,l.clerk_id, l.employee_id,e.employee_name,e.room_no, e.dept_name, e.phone_no, l.total_cost from log_details l, employee e where e.employee_id=l.employee_id;";
+			rs = st.executeQuery(query);
+			while(rs.next())
+			{
+				rs.getString(i++);           	
+			}
 
-    public void updateDeliveryStatus(String print_id) {
-        Connection connection = getConnection();
-        Statement st = null;
-        try {
-            st = connection.createStatement();
-            String qeury = "update print_details set status = 'Delivered' where print_id = '" + print_id + "'";
-            st.executeUpdate(qeury);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}   
+
+	public int updateDeliveryStatus(String print_id,String option) {
+
+		String query1 = "UPDATE log_details SET delivery_status='" + option +"' WHERE print_id='" + print_id + "' AND delivery_status='To be delivered';";
+		try {
+			Connection connection = getConnection();
+			Statement stmt = connection.createStatement();
+			int c = stmt.executeUpdate(query1);	
+			if(c==1)
+			{
+				return 1;
+			}
+			else
+				return -1;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return -1;
+		}
+
+	}
+
+
 
 }
+
+
